bbcb15303fe41b95ded6dea99a03b46d98d76c1a
kiplimoboor <kiplimoarapboor@gmail.com>
2024-05-02 15:29:29 +0300

    float to str

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -3,4 +3,12 @@
 
 
 def to_str(n: float) -> str:
+    """
+    Converts float to str
+
+    Args:
+        n[float]: the float
+    Return:
+        [str]: the string version of the float
+    """
     return f"{n}"
