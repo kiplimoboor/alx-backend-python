74cb4296a3479fb8c42daeacddc1cef8fa701e38
kiplimoboor <kiplimoarapboor@gmail.com>
2024-05-02 12:39:25 +0300

    concat annotated

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,4 +3,13 @@
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    A simple string concatenator function
+
+    Args:
+        str1[str]: the first string
+        str2[str]: the second string
+    Return:
+        [str]: a concatenation of str1 and str2
+    """
     return str1 + str2
