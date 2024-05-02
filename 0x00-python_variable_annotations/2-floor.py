95a4422ae12c1fb4bc2c4930a95cdc16d4187210
kiplimoboor <kiplimoarapboor@gmail.com>
2024-05-02 13:11:14 +0300

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A simple float flooring module"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Finds floor of a float
+
+    Args:
+        n[float]: the float to be floored
+    Return:
+        [int]: the result of n being floored
+    """
+    return math.floor(n)
