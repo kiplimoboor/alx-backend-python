c29e6948612fe6320cc985535920d4df7294a216
kiplimoboor <kiplimoarapboor@gmail.com>
2024-05-07 16:11:16 +0300

    yield async

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,16 +3,15 @@
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     A simple async generator
     Return:
         Yields a random number 10 times
     """
-    i = 0
-    while i < 10:
-        i += 1
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
